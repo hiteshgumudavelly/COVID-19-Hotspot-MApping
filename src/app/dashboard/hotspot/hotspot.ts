7e4a501ba8e0f91a3074ed9ae5a8efbebf2b0103
hiteshgumudavelly <30660906+hiteshgumudavelly@users.noreply.github.com>
2021-07-13 19:02:16 +1000

    Add files via upload

---
+++ src/app/dashboard/hotspot/hotspot.ts
@@ -0,0 +1,22 @@
+
+
+export class Hotspot {
+
+    id: number = 0;
+    siteName: string = ''; //site
+    address: string = ''; //site
+    suburbName: string = ''; //location
+    postcode: string = '';
+    state: string = '';
+    country: string = '';
+    latitude: number = 0;
+    longitude: number = 0
+    startDate: Date = new Date();
+    endDate: Date = new Date();
+    note: string = '';
+    constructor(
+    ) {
+
+    }
+
+}
